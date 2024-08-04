f5d8d21836bfad9dba303286f77860bb6f43a046
Devajit <patar1447@gmail.com>
2024-08-04 20:26:09 +0530

    BluetoothReceiver implemented for States (State on/off and State turing on/off) and also unregister the receiver

--- app/src/main/java/com/developer/android/dev/technologia/androidapp/bluetooth_xml/BluetoothReceiver.kt
+++ app/src/main/java/com/developer/android/dev/technologia/androidapp/bluetooth_xml/BluetoothReceiver.kt
@@ -4,13 +4,28 @@ import android.bluetooth.BluetoothAdapter
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 
 class BluetoothReceiver:BroadcastReceiver(){
     override fun onReceive(context: Context?, intent: Intent?) {
        val action = intent?.action
         if(action == BluetoothAdapter.ACTION_STATE_CHANGED){
-            when(intent.action){
+            when(intent.getIntExtra(BluetoothAdapter.EXTRA_STATE,BluetoothAdapter.ERROR)){
+                BluetoothAdapter.STATE_ON ->{
+                    Log.d("Message","State On")
+                }
 
+                BluetoothAdapter.STATE_OFF ->{
+                    Log.d("Message","State Off")
+                }
+
+                BluetoothAdapter.STATE_TURNING_ON ->{
+                    Log.d("Message","State Turning On")
+                }
+
+                BluetoothAdapter.STATE_TURNING_OFF ->{
+                    Log.d("Message","State Turning Off")
+                }
             }
         }
     }
--- app/src/main/java/com/developer/android/dev/technologia/androidapp/bluetooth_xml/MainActivity.kt
+++ app/src/main/java/com/developer/android/dev/technologia/androidapp/bluetooth_xml/MainActivity.kt
@@ -85,4 +85,9 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(receiver)
+    }
+
 }
